5ff5c7ce8815d4191a47503ece4a4f45011faf8b
Noah Excellence <62033363+Arkilicious@users.noreply.github.com>
2025-04-09 01:36:19 +0100

    Add sentinel-trace/server/auth_app/admin.py 1744158978128

---
+++ sentinel-trace/server/auth_app/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'role', 'is_staff', 'is_active')
+    list_filter = ('role', 'is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Security Info', {'fields': ('role', 'cac_id', 'otp_secret', 'is_verified')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'role', 'cac_id'),
+        }),
+    )
+    search_fields = ('username', 'email', 'cac_id')
+    ordering = ('username',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
